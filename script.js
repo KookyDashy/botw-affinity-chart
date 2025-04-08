2bd942f329f70d6237107a3e05f3f25138f370d9
KookyDashy <kookydashy57@gmail.com>
2025-04-08 14:37:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function showTooltip(e, text) {
+function showTooltip(e, element) {
+  const name = element.getAttribute("data-name");
+  const subtitle = element.getAttribute("data-subtitle");
+  const race = element.getAttribute("data-race");
+  const location = element.getAttribute("data-location");
+
   const tooltip = document.getElementById("tooltip");
-  tooltip.innerText = text;
+  tooltip.innerHTML = `
+    <strong>${name}</strong><br>
+    <em>${subtitle}</em><br>
+    Race: ${race}<br>
+    Location: ${location}
+  `;
+
   tooltip.style.display = "block";
   tooltip.style.left = e.pageX + 10 + "px";
   tooltip.style.top = e.pageY + 10 + "px";
 }
 
 function hideTooltip() {
-  document.getElementById("tooltip").style.display = "none";
+  const tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "none";
 }
+
