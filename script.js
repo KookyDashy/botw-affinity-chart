b475dff7b9b274d88d9df15237b489f1d4ccd174
KookyDashy <kookydashy57@gmail.com>
2025-04-08 11:49:04 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showTooltip(e, text) {
+  const tooltip = document.getElementById("tooltip");
+  tooltip.innerText = text;
+  tooltip.style.display = "block";
+  tooltip.style.left = e.pageX + 10 + "px";
+  tooltip.style.top = e.pageY + 10 + "px";
+}
+
+function hideTooltip() {
+  document.getElementById("tooltip").style.display = "none";
+}
